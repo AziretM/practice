d3072526612aaf5d82af53084164b5b93b955e8b
AziretM <aziret897969@gmail.com>
2023-03-19 16:05:20 +0600

    prac

---
+++ script.js
@@ -0,0 +1,259 @@
+// const body = document.body
+
+// const name = document.createElement('h3')
+// name.innerHTML = `Имя:`
+// body.prepend(name)
+
+// const inputN = document.createElement('input')
+// inputN.placeholder = 'Введите имя'
+// body.append(inputN)
+
+// const name__two = document.createElement('h3')
+// name__two.innerHTML = `Фамилие:`
+// body.append(name__two)
+
+// const inputN__two = document.createElement('input')
+// inputN__two.placeholder = 'Введите фамилие'
+// body.append(inputN__two)
+
+// const radioOne = document.createElement('h3')
+// radioOne.innerHTML=`Мужчина`
+// body.append(radioOne)
+
+// const al = document.createElement('input')
+// al.type = 'radio'
+// body.append(al)
+
+// const radio = document.createElement('h3')
+// radio.innerHTML=`Женщина`
+// body.append(radio)
+
+// const radioTwo = document.createElement('input')
+// radioTwo.type = "radio"
+// body.append(radioTwo)
+
+// const about = document.createElement('h3')
+// about.innerHTML=`О себе:`
+// body.append(about)
+
+// const check = document.createElement('input')
+// check.type = "checkbox"
+// body.append(check)
+
+// const yes = document.createElement('h3')
+// yes.innerHTML=`Меньше 18`
+// body.append(yes)
+
+// const check__one = document.createElement('input')
+// check__one.type = "checkbox"
+// body.append(check__one)
+
+// const yes__one = document.createElement('h3')
+// yes__one.innerHTML=`от 18 до 35`
+// body.append(yes__one)
+
+// const check__two = document.createElement('input')
+// check__two.type = "checkbox"
+// body.append(check__two)
+
+// const yes__two = document.createElement('h3')
+// yes__two.innerHTML=`Больше 35`
+// body.append(yes__two)
+
+// const check__three = document.createElement('input')
+// check__three.type = "checkbox"
+// body.append(check__three)
+
+// const yes__three = document.createElement('h3')
+// yes__three.innerHTML=`Женат/-a`
+// body.append(yes__three)
+
+// const check__four = document.createElement('input')
+// check__four.type = "checkbox"
+// body.append(check__four)
+
+// const yes__four = document.createElement('h3')
+// yes__four.innerHTML=`Есть кошка`
+// body.append(yes__four)
+
+// const check__five = document.createElement('input')
+// check__five.type = "checkbox"
+// body.append(check__five)
+
+// const btn = document.createElement('button')
+// btn.innerHTML = `Name`
+// body.append(btn)
+
+
+// Второй
+
+// const body = document.body
+
+// const mainDiv = document.createElement('div')
+// mainDiv.classList.add('main__div')
+// mainDiv.style.border = '1px solid hsl(4.21deg 89.06% 74.9%)'
+// mainDiv.style.width = '30%'
+// mainDiv.style.padding = '1rem '
+// body.prepend(mainDiv)
+
+// const name = document.createElement('h3')
+// name.innerHTML= `Name:`
+// body.append(name)
+
+// const input = document.createElement('input')
+// input.placeholder ='name'
+// input.style.backgroundColor ='#D1CBC7'
+// input.style.width = '20rem'
+// input.style.height = '3rem'
+// input.style.borderRadius = '0.3rem'
+// input.style.border = 'none'
+// input.style.color = 'red'
+// body.append(input)
+
+// const name__two = document.createElement('h3')
+// name__two.innerHTML= `Number:`
+// body.append(name__two)
+
+// const input__two = document.createElement('input')
+// input__two.placeholder ='number'
+// input__two.style.backgroundColor ='#D1CBC7'
+// input__two.style.width = '20rem'
+// input__two.style.height = '3rem'
+// input__two.style.borderRadius = '0.3rem'
+// input__two.style.border = 'none'
+// input__two.style.color = 'red'
+// body.append(input__two)
+
+// const name__three = document.createElement('h3')
+// name__three.innerHTML= `Email`
+// body.append(name__three)
+
+// const input__three = document.createElement('input')
+// input__three.placeholder ='email'
+// input__three.style.backgroundColor ='#D1CBC7'
+// input__three.style.width = '20rem'
+// input__three.style.height = '3rem'
+// input__three.style.borderRadius = '0.3rem'
+// input__three.style.border = 'none'
+// input__three.style.color = 'red'
+// body.append(input__three)
+
+// const name__four = document.createElement('h3')
+// name__four.innerHTML= `Sum`
+// body.append(name__four)
+
+// const input__four = document.createElement('input')
+// input__four.placeholder ='email'
+// input__four.style.backgroundColor ='#D1CBC7'
+// input__four.style.width = '20rem'
+// input__four.style.height = '3rem'
+// input__four.style.borderRadius = '0.3rem'
+// input__four.style.border = 'none'
+// input__four.style.color = 'red'
+// body.append(input__four)
+
+// const btn = document.createElement('button')
+// btn.innerHTML = `Name`
+// btn.style.backgroundColor = 'red'
+// btn.style.width = '10rem'
+// btn.style.height = '3rem'
+// btn.style.marginTop = '20px'
+
+// btn.style.border = 'none'
+// btn.style.borderRadius = '0.3rem'
+// btn.style.color = 'white'
+// btn.style.display = 'block'
+// body.append(btn)
+
+
+// mainDiv.append(name)
+// mainDiv.append(input)
+// mainDiv.append(name__two)
+// mainDiv.append(input__two)
+// mainDiv.append(name__three)
+// mainDiv.append(input__three)
+// mainDiv.append(name__four)
+// mainDiv.append(input__four)
+// mainDiv.append(btn)
+
+
+// третий
+
+// const body = document.body
+
+// const name = document.createElement('p')
+// name.innerHTML= `Фамилия Имя:`
+// name.style.fontSize = '20px'
+// body.append(name)
+
+// const input = document.createElement('input')
+// input.style.width = '26rem'
+// input.style.height = '2.5rem'
+// input.style.border = '1px solid grey'
+// input.style.color = 'red'
+// body.append(input)
+
+// const name__two = document.createElement('p')
+// name__two.innerHTML= `Введите номер заказа:`
+// name__two.style.fontSize = '20px'
+// body.append(name__two)
+
+// const input__two = document.createElement('input')
+// input__two.style.width = '26rem'
+// input__two.placeholder = 'Оставьте сообщение здесь'
+// input__two.style.height = '2.5rem'
+// input__two.style.border = '1px solid grey'
+// input__two.style.color = 'red'
+// body.append(input__two)
+
+// const name__three = document.createElement('p')
+// name__three.innerHTML= `Телефон:`
+// name__three.style.fontSize = '20px'
+// body.append(name__three)
+
+// const input__three = document.createElement('input')
+// input__three.style.width = '26rem'
+// input__three.style.height = '2.5rem'
+// input__three.style.border = '1px solid grey'
+// input__three.style.color = 'red'
+// body.append(input__three)
+
+// const name__four = document.createElement('p')
+// name__four.innerHTML= `Электронный адрес:`
+// name__four.style.fontSize = '20px'
+// body.append(name__four)
+
+// const input__four = document.createElement('input')
+// input__four.style.width = '26rem'
+// input__four.style.height = '2.5rem'
+// input__four.placeholder = 'test@example.com'
+
+// input__four.style.border = '1px solid grey'
+// input__four.style.color = 'red'
+// body.append(input__four)
+
+// const name__five = document.createElement('p')
+// name__five.innerHTML= `Введите сумму оплаты:`
+// name__five.style.fontSize = '20px'
+// body.append(name__five)
+
+// const input__five = document.createElement('input')
+// input__five.style.width = '26rem'
+// input__five.style.height = '2.5rem'
+// input__five.style.border = '1px solid grey'
+// input__five.style.color = 'red'
+// body.append(input__five)
+
+
+// const submit = document.createElement("button")
+// submit.innerHTML = `Перейдите к оплате`
+// submit.style.marginTop = '20px'
+// submit.style.fontSize = '20px'
+// submit.style.backgroundColor = 'pink'
+// submit.style.border = 'none'
+// submit.style.width = '15rem'
+// submit.style.height = '5rem'
+// submit.style.display = 'block'
+// submit.style.color = 'white'
+
+// body.append(submit)
